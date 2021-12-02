97ef96efebd09aba3aae38257830a3160cdc8107
inkinvissible <agusmartin113@gmail.com>
2021-12-02 09:05:29 -0300

    Todo commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+
+
+
+let nombre=prompt("Ingrese su nombre");
+let edad=prompt("Ingrese su edad")
+alert("Que tengas un lindo día "+nombre);
+let condicion=prompt("Es tu edad "+edad+"?")
+if (condicion==="si" || "Si" || "sí" || "Sí"){
+    alert("Muchas gracias! Buen día!")
+}else{
+    alert("Ups...")
+}
