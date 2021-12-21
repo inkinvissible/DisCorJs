314b8eaa709f2a33e40fe3a73cca144a74302cef
inkinvissible <agusmartin113@gmail.com>
2021-12-21 08:48:28 -0300

    Objetos y Clases

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
+class Usuario {
+    constructor(nombre, apellido, telefono, email) {
+        this.nombre = nombre.toLowerCase();
+        this.apellido = apellido.toLowerCase();
+        this.telefono = telefono;
+        this.email = email;
+
+    }
+    verificacion() {
+        while ((!this.email.includes("@")) && (!this.email.includes(".com"))) {
+            alert("Su email es inválido");
+            this.email=prompt("Ingrese nuevamente su email");
+        }
+
+    }
+    getDatos() {
+        alert("Sus datos son: " + this.nombre + "\n" + this.apellido + "\n" + this.telefono + "\n" + this.email);
+    }
+}
+alert("Le pediremos que ingrese sus datos");
+const usuario1 = new Usuario(
+    prompt("Ingrese su nombre"),
+    prompt("Ingrese su apellido"),
+    prompt("Ingrese su teléfono"),
+    prompt("Ingrese su Email")
+);
+usuario1.verificacion();
+usuario1.getDatos();
+
 
 
 const productoSelected = (producto) => {
@@ -7,7 +36,8 @@ const productoSelected = (producto) => {
     return producto;
 }
 const comprobacion = (seleccion) => {
-    if ((seleccion === "Manijas") || (seleccion === "manijas")) {
+    seleccion = seleccion.toLowerCase();
+    if (seleccion === "manijas") {
         let presupuesto = parseInt(prompt("Seleccione su presupuesto para las " + seleccion));
         while (presupuesto <= 1500) {
             alert("Su presupuesto es muy chico, por favor, ingrese una nueva cantidad de dinero superior a $1.500");
@@ -15,54 +45,57 @@ const comprobacion = (seleccion) => {
         }
         alert("Su presupuesto es correcto")
 
-        alert("El precio de costo de " + seleccion + " es de $" + (presupuesto-((presupuesto*10)/100)));
+        alert("El precio de costo de " + seleccion + " es de $" + (presupuesto - ((presupuesto * 10) / 100)));
         const iva = () => {
             let ivaVariable = presupuesto * 0.21;
             presupuesto += ivaVariable;
 
         }
         const salidaIva = () => {
-            alert("Su precio final es: $"+presupuesto);
+            alert("Su precio final es: $" + presupuesto);
         }
         salidaIva(iva());
     }
-    if ((seleccion === "Cerraduras") || (seleccion === "cerraduras")) {
+    if (seleccion === "cerraduras") {
         let presupuesto = parseInt(prompt("Seleccione su presupuesto para las " + seleccion));
         while (presupuesto <= 2500) {
             alert("Su presupuesto es muy chico, por favor, ingrese una nueva cantidad de dinero superior a $2.500");
             presupuesto = parseInt(prompt("Seleccione su presupuesto para las " + seleccion));
         }
         alert("Su presupuesto es correcto");
-        alert("El precio de costo de " + seleccion + " es de $" + (presupuesto-((presupuesto*10)/100)));
+        alert("El precio de costo de " + seleccion + " es de $" + (presupuesto - ((presupuesto * 10) / 100)));
         const iva = () => {
             let ivaVariable = presupuesto * 0.21;
             presupuesto += ivaVariable;
 
         }
         const salidaIva = () => {
-            alert("Su precio final es: $"+presupuesto);
+            alert("Su precio final es: $" + presupuesto);
         }
         salidaIva(iva());
     }
-    if ((seleccion === "máquinas") || (seleccion === "maquinas") || (seleccion === "Máquinas") || (seleccion === "Maquinas")) {
+    if ((seleccion === "máquinas") || (seleccion === "maquinas")) {
         let presupuesto = parseInt(prompt("Seleccione su presupuesto para las " + seleccion));
         while (presupuesto <= 3500) {
             alert("Su presupuesto es muy chico, por favor, ingrese una nueva cantidad de dinero superior a $3.500");
             presupuesto = parseInt(prompt("Seleccione su presupuesto para las " + seleccion));
         }
         alert("Su presupuesto es correcto");
-        alert("El precio de costo de " + seleccion + " es de $" + (presupuesto-((presupuesto*10)/100)));
+        alert("El precio de costo de " + seleccion + " es de $" + (presupuesto - ((presupuesto * 10) / 100)));
         const iva = () => {
             let ivaVariable = presupuesto * 0.21;
             presupuesto += ivaVariable;
 
         }
         const salidaIva = () => {
-            alert("Su precio final es: $"+presupuesto);
+            alert("Su precio final es: $" + presupuesto);
         }
         salidaIva(iva());
     }
 }
 
 comprobacion(productoSelected(prompt("Escriba los productos. Los productos son los siguientes \n1. Manijas \n2. Máquinas\n3.Cerraduras")));
+
+
+
 
